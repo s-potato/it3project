8bcca30d1e24efaa1f2465ebbcbdef8e82ef2a57
s-potato <51790227+s-potato@users.noreply.github.com>
2021-12-11 17:24:54 +0700

    add bootstrap2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :film_reviews
   resources :films
   get 'users/show'
-  root 'static_pages#home'
+  root 'films#index'
   get 'static_pages/home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
